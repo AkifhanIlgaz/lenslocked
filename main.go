c545d57f9887ee670e0a42db4e58a67c5b1df386
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-08 16:00:34 +0300

    add contact page handler

--- main.go
+++ main.go
@@ -7,17 +7,29 @@ import (
 
 // Check https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers for all available HTTP headers
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Header is just a map.  type Header map[string][]string
 	// Set() replaces any existing values for the key, Add() appends to existing values
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, `<h1>Welcome to my awesome site</h1>`)
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `
+	 <h1>
+	 Contact Page 
+	  </h1>
+
+	 <p>To get in touch, email me at <a href="mailto:akifhanilgaz@gmail.com">akifhanilgaz@gmail.com</a></p>
+	
+	`)
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handlerFunc)
-
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000")
 
 	http.ListenAndServe(":3000", mux)
