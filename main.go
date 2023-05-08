e3ee286bef6be7bd8512b72debafca96720b42f0
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-08 15:38:48 +0300

    comment out difference between Set() and Add() function of Header

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 // Check https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers for all available HTTP headers
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	// Header is just a map.  type Header map[string][]string
+	// Set() replaces any existing values for the key, Add() appends to existing values
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, `<h1>Welcome to my awesome site</h1>`)
 }
