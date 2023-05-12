69a5730d3ebb48508a2d53e4e4ca5e4464e9e901
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-12 15:11:18 +0300

    use range action in template

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 type User struct {
-	Name  string
-	Age   int
-	Score int
+	Name    string
+	Age     int
+	Scores  []float64
+	Aliases map[string]string
 }
 
 func main() {
@@ -19,9 +20,14 @@ func main() {
 	}
 
 	user := User{
-		Name:  "Zozak",
-		Age:   111,
-		Score: 77,
+		Name:   "Zozak",
+		Age:    111,
+		Scores: []float64{1.2, 3.4, 0.5, 77.3},
+		Aliases: map[string]string{
+			"Zozak":    "ZZK",
+			"Starknet": "STRK",
+			"Aptos":    "APT",
+		},
 	}
 
 	err = t.Execute(os.Stdout, user)
