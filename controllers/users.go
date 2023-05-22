f83a322d9ff10c585df57dd62301100477af9e29
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-22 10:31:51 +0300

    use interface to avoid importing views package

--- controllers/users.go
+++ controllers/users.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"net/http"
-
-	"github.com/AkifhanIlgaz/lenslocked/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
