d7dc48dd86da03dde0ec28776e9a78c8e328647e
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-30 12:30:44 +0300

    create sign in page template

--- main.go
+++ main.go
@@ -37,8 +37,10 @@ func main() {
 		UserService: &userService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup_go.html", "tailwind_go.html"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin_go.html", "tailwind_go.html"))
 	r.Get("/signup", usersC.New)
 	r.Post("/signup", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		// w.WriteHeader(http.StatusNotFound)
