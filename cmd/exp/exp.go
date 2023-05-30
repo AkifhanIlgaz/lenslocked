630b31bc5708e8c59d6e36750d357528033fc3f6
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-30 11:54:38 +0300

    added Postgres Config for models package

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,37 +1,13 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/AkifhanIlgaz/lenslocked/models"
-	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
-}
-
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "baloo",
-		Password: "junglebook",
-		DBName:   "lenslocked",
-		SSLMode:  "disable",
-	}
-
-	db, err := sql.Open("pgx", cfg.String())
+	db, err := models.Open(models.DefaultPostgresConfig())
 	if err != nil {
 		panic(err)
 	}
@@ -47,7 +23,7 @@ func main() {
 		DB: db,
 	}
 
-	jon, err := userService.Create("j2on@calhoun.io", "secret-password")
+	jon, err := userService.Create("j2o2n@calhoun.io", "secret-password")
 	if err != nil {
 		panic(err)
 	}
