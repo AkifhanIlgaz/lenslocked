7bdcfc6d11ca70001768b69668b2dafb3db4c0e1
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-30 10:11:34 +0300

    compare password and hash

--- cmd/bcrypt/bcrypt.go
+++ cmd/bcrypt/bcrypt.go
@@ -10,24 +10,29 @@ import (
 func main() {
 	switch os.Args[1] {
 	case "hash":
-		fmt.Println(hash(os.Args[2]))
+		hash(os.Args[2])
 	case "compare":
-		fmt.Println(compare(os.Args[2], os.Args[3]))
+		compare(os.Args[2], os.Args[3])
 	default:
 		fmt.Println("Invalid command", os.Args[1])
 	}
 }
 
-func hash(password string) string {
+func hash(password string) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		fmt.Println("error hashing: ", password)
-		return ""
+		return
 	}
 
-	return string(hashedPassword)
+	fmt.Println(string(hashedPassword))
 }
 
-func compare(password, hash string) error {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+func compare(password, hash string) {
+	if bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) != nil {
+		fmt.Println("Password is invalid: ", password)
+		return
+	}
+
+	fmt.Println("Password is correct!")
 }
