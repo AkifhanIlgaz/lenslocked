f78ca7d9144d33c1282b1176608eca059f766d8c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-06-09 11:26:00 +0300

    sql indexes

--- models/sql/query.sql
+++
@@ -1,6 +0,0 @@
-SELECT users.id,
-    users.email,
-    users.password_hash
-FROM sessions
-    JOIN users ON users.id = sessions.user_id
-WHERE sessions.token_hash = '0M3jdFRpoBY6fQoRvDSQzZbgNwYnI4dLEGUtLsxnSBA=';
