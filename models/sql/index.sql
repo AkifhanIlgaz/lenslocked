c8a57970c7a7b2d6d1e2407636f9ba1678ad0175
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-06-19 16:18:59 +0300

    remove old sql files

--- models/sql/index.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX sessions_token_hash_idx ON sessions(token_hash);
-
-CREATE TABLE dogs(
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    user_id REFERENCES users(id) ON DELETE CASCADE,
-    -- Does NOT work with Postgres
-    INDEX dogs_user_id_idx (user_id)
-);
