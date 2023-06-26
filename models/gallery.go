27bedb2131a73791c5a1daed230df32b080401f4
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-06-26 15:02:27 +0300

    implement galleryService.Create method

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,39 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Gallery struct {
 	Id     int
 	UserId int
 	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (service *GalleryService) Create(userId int, title string) (*Gallery, error) {
+	gallery := Gallery{
+		UserId: userId,
+		Title:  title,
+	}
+
+	row := service.DB.QueryRow(`
+		INSERT INTO galleries (user_id, title)
+		VALUES (
+			$1,
+			$2
+		)
+		RETURNING id;
+	`, userId, title)
+
+	err := row.Scan(&gallery.Id)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+
+	return &gallery, nil
 }
