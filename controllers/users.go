38dc80e9ff546899d72aa3ef4cc4342522d10121
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-10 10:30:48 +0300

    redirect to galleries after authentication

--- controllers/users.go
+++ controllers/users.go
@@ -60,7 +60,7 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 
 	setCookie(w, CookieSession, session.Token)
 
-	http.Redirect(w, r, "users/me", http.StatusFound)
+	http.Redirect(w, r, "/galleries", http.StatusFound)
 }
 
 func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
@@ -98,7 +98,7 @@ func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
 
 	setCookie(w, CookieSession, session.Token)
 
-	http.Redirect(w, r, "users/me", http.StatusFound)
+	http.Redirect(w, r, "/galleries", http.StatusFound)
 }
 
 func (u Users) ProcessSignOut(w http.ResponseWriter, r *http.Request) {
